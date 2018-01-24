8cb71fd03acf4d09a6a7f461663780c3cc4b89a3
Michael Scopchanov <michael_scopchanov@yahoo.com>
2018-01-24 21:27:44 +0100

    Initial commit

---
+++ MainWindow.h
@@ -0,0 +1,18 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+class MainWindow : public QMainWindow
+{
+	Q_OBJECT
+public:
+	explicit MainWindow(QWidget *parent = nullptr);
+
+private slots:
+	void onAlert();
+	void onMessage();
+	void onDrumset();
+};
+
+#endif // MAINWINDOW_H
